60bcf1da62adfb2487b4f45b50c4c28988902885
rohanpatel01 <65795794+rohanpatel01@users.noreply.github.com>
2023-04-15 22:49:23 -0500

    Add files via upload

--- SpaceInvaders.c
+++ SpaceInvaders.c
@@ -33,10 +33,38 @@
 #include "Images.h"
 #include "../inc/wave.h"
 #include "Timer1.h"
+#include <math.h>
+#include "../inc/wave.h"
+#include "../inc/DAC.h"
+
 
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
 void Delay100ms(uint32_t count); // time delay in 0.1 seconds
+void numToLCD(int num);				// print a number to the LCD
+
+
+struct sprite {
+  int32_t x;      // x coordinate
+  int32_t y;      // y coordinate
+	int32_t oldX;      
+  int32_t oldY;
+  int32_t vy, vx;  // pixels/30Hz
+	int32_t originalVy;
+  const unsigned short *image; // ptr->image
+  const unsigned short *black;
+  int32_t life;        // dead (0) : alive (1)
+  int32_t w; // width
+  int32_t h; // height
+	
+	int32_t blackW; // width
+  int32_t blackH; // height
+	
+};
+typedef struct sprite sprite_t;
+
+
+int Collision(sprite_t sprite1, sprite_t sprite2);
 
 
 int main1(void){
@@ -103,11 +131,34 @@ const char Language_English[]="English";
 const char Language_Spanish[]="Espa\xA4ol";
 const char Language_Portuguese[]="Portugu\x88s";
 const char Language_French[]="Fran\x87" "ais";
+
+// 4E3A
+// Press UP for English
+char* english_Selection = "Press UP for English";
+char* french_Selection = "Vers le bas francais";
+
+char* Score_English = "Score: ";
+
+char* english_jump_Instruction = "Press UP to jump";
+char* english_shoot_Instruction = "Press RIGHT to Shoot";
+char* english_float_Instruction = "Hold DOWN to float";
+char* english_objective_Instruction = "GL - DON'T DIE";
+char* english_continue_Instruction = "UP now to continue";
+
+
+char* french_jump_Instruction = "Jusqu'\xA0 SAUTER";
+char* french_shoot_Instruction = "Droit de TIRRR";
+char* french_float_Instruction = "DESCEND pour flotter";
+char* french_objective_Instruction = "GLE - ne meurs pas";
+char* french_continue_Instruction = "UP pour continuer";
+
 const char *Phrases[3][4]={
   {Hello_English,Hello_Spanish,Hello_Portuguese,Hello_French},
   {Goodbye_English,Goodbye_Spanish,Goodbye_Portuguese,Goodbye_French},
   {Language_English,Language_Spanish,Language_Portuguese,Language_French}
 };
+
+
 
 // global variables
 uint32_t Data;        // 12-bit ADC
@@ -193,24 +244,6 @@ uint32_t Convert(int32_t x){
 }
 
 
-struct sprite {
-  int32_t x;      // x coordinate
-  int32_t y;      // y coordinate
-	int32_t oldX;      
-  int32_t oldY;
-  int32_t vy, vx;  // pixels/30Hz
-	int32_t originalVy;
-  const unsigned short *image; // ptr->image
-  const unsigned short *black;
-  int32_t life;        // dead (0) : alive (1)
-  int32_t w; // width
-  int32_t h; // height
-	
-	int32_t blackW; // width
-  int32_t blackH; // height
-	
-};
-typedef struct sprite sprite_t;
 
  //{playerX, playerY, playerVelX, playerVelY, player, playerBlack, 1, 20, 21, 1};
 sprite_t playerSprite; // player is a global variable b/c want other functions to access info
@@ -218,6 +251,15 @@ sprite_t playerShootSprite;
 sprite_t enemySprite;
 
 uint32_t valueOut = 0;
+int gameOverFlag = 0;
+int enemyKilledFlag = 0;
+
+uint32_t scoreNum = 0;
+char scoreNumString;
+
+int languageSelectionFlag = 0;
+int tutorialFlag = 0;
+int isEnglish = 0;
 
 
 // go to handler
@@ -267,9 +309,32 @@ void Timer3A_Handler(void){
 		//playerSprite.vy = originalVY; // changed on float
 	}
 	
-	if (currentStateJump != 0 && lastStateJump == 0 && playerSprite.y == 126){
+	// finish tutorial
+	if (!tutorialFlag && currentStateJump != 0 && lastStateJump == 0 && languageSelectionFlag){
+		GPIO_PORTD_DATA_R ^= 0x02;
+		tutorialFlag = 1;
+	}
+	
+	// select english
+	if (!languageSelectionFlag && currentStateJump != 0 && lastStateJump == 0 ){
+		GPIO_PORTD_DATA_R ^= 0x02;
+		isEnglish = 1;
+		languageSelectionFlag = 1; // turn flag 1 - move past language selection
+	}
+	
+	if (!languageSelectionFlag && currentStateFloat != 0 ){
+		GPIO_PORTD_DATA_R ^= 0x02;
+		isEnglish = 0; // language is French
+		languageSelectionFlag = 1;
+	}
+	
+	
+	
+	
+	if (currentStateJump != 0 && lastStateJump == 0 && playerSprite.y == 126 && tutorialFlag){
 		GPIO_PORTD_DATA_R ^= 0x02; // toggle led on button input
 		jumpFlag = 1;
+		Wave_Jump();
 	}
 	
 	if (jumpFlag){
@@ -291,10 +356,11 @@ void Timer3A_Handler(void){
 	// shooting code
 	// have another flag that only lets you shoot once the previous shot is dead
 	
-	if (currentStateShoot != 0 && lastStateShoot == 0 && !shootFlag){
+	if (currentStateShoot != 0 && lastStateShoot == 0 && !shootFlag && tutorialFlag){
 		GPIO_PORTD_DATA_R ^= 0x01; // toggle led on button input
 		playerShootSprite.life = 1;
 		shootFlag = 1;
+		Wave_Shoot();
 		
 		// spell spawn location
 		playerShootSprite.x = playerSprite.x + playerSprite.w + 2; // spawn fireball infront of player
@@ -312,9 +378,13 @@ void Timer3A_Handler(void){
 			playerShootSprite.life = 0; // stop drawing
 			shootFlag = 0;						  // stop moving
 		}
+		
+		
+			
+		
 	}
 		
-	if (currentStateFloat != 0 && playerSprite.y < 126){
+	if (currentStateFloat != 0 && playerSprite.y < 126 && tutorialFlag){ 
 		playerSprite.vy = 1;
 		
 	} else {
@@ -330,14 +400,14 @@ void Timer3A_Handler(void){
 	// if 2 is greater then we will change more often
 	// changed xy to vx
 	// changed < to >
-	if (randomValue > maxValue - 4 ){
+	if ((randomValue > maxValue - 4) && tutorialFlag){ 
 		enemySprite.vx *= -1;
 
 	}
 	
 	// restrict enemy
 	// (160 - enemySprite.w)
-	if ( !( (enemySprite.x + enemySprite.vx) > 128 ) && ( !( (enemySprite.x + enemySprite.vx) < enemySprite.w)) ){
+	if ( !( (enemySprite.x + enemySprite.vx) > 128 ) && ( !( (enemySprite.x + enemySprite.vx) < enemySprite.w)) && tutorialFlag){
 		enemySprite.x += enemySprite.vx;
 		
 	} else if (enemySprite.x >= 128 || enemySprite.x <= enemySprite.w){ // if player is at edge immeditely move different direction
@@ -345,9 +415,19 @@ void Timer3A_Handler(void){
 		
 	}
 	
-	
-	
+	// detect collision between player and enemy --> end game if true
+	if ( Collision(playerSprite, enemySprite) ){
+		// toggle led if overlap
+		GPIO_PORTD_DATA_R ^= 0x01;
+		gameOverFlag = 1;
+	}
 
+	if (Collision(playerShootSprite, enemySprite)){
+		GPIO_PORTD_DATA_R ^= 0x01;
+		enemyKilledFlag = 1;
+		
+	}
+	
 	lastStateFloat = currentStateFloat;
 	lastStateShoot = currentStateShoot;
 	lastStateJump = currentStateJump;
@@ -356,30 +436,28 @@ void Timer3A_Handler(void){
 }
 
 
-
 int main(void){ 
 	char l;
   DisableInterrupts();
   TExaS_Init(NONE);       		// Bus clock is 80 MHz 
   Output_Init();
-	ST7735_InitR(INITR_REDTAB);
+	ST7735_InitR(INITR_BLACKTAB); //INITR_REDTAB
 	ADC_Init();	
 	PortF_Init();	//
 	PortE_Init(); // switch 
 	PortD_Init(); // LED
+	Wave_Init(); // initializes timer2A for sound and does DAC init
 	
 	Timer3A_Init( 1333333, 5); // 10hz = 8000000   : 30hz = 2666667  : 60hz = 1333333  : 120hz 666667
   Timer1_Init(  1333333, 5); // sample button at 30Hz
 	
 	ST7735_FillScreen(0x0000);
 	ST7735_SetRotation(3);
-
-	// variables for game
 	
 	int32_t playerMovedFlag = 0;
 	
 	// initialize player
-	playerSprite.x = 30;
+	playerSprite.x = 0;
 	playerSprite.y = 126;
 	playerSprite.oldX = 0;
 	playerSprite.oldY = 126; // change
@@ -423,13 +501,57 @@ int main(void){
 	enemySprite.blackH = 21;
 	enemySprite.life = 1;
 	
-	// seed random number generator	
 	Random_Init(NVIC_ST_CURRENT_R);
 
 	EnableInterrupts();
+	
+	// language selection
+	while (!languageSelectionFlag){
+		ST7735_DrawString(1, 3, english_Selection , 255);
+		ST7735_DrawString(1, 7, french_Selection , 255);
+	}
+	
+	// title screen for game - create and display image 
+	
+	ST7735_FillScreen(0x0000);
+	Delay100ms(10);
+	
+	while (!tutorialFlag){
+		
+		if (isEnglish){
+			
+			ST7735_DrawString(1, 1, english_jump_Instruction , 255);
+			ST7735_DrawString(1, 2, english_shoot_Instruction , 255);
+			ST7735_DrawString(1, 3, english_float_Instruction , 255);
+			ST7735_DrawString(1, 4, english_objective_Instruction , 255);
+			ST7735_DrawString(1, 5, english_continue_Instruction , 255);
 
-
-	while(1){
+			
+			// french_continue_Instruction
+			
+		} else {
+			
+			ST7735_DrawString(1, 1, french_jump_Instruction , 255);
+			ST7735_DrawString(1, 2, french_shoot_Instruction , 255);
+			ST7735_DrawString(1, 3, french_float_Instruction , 255);
+			ST7735_DrawString(1, 4, french_objective_Instruction , 255);
+			ST7735_DrawString(1, 5, french_objective_Instruction , 255);
+			
+		}
+	}
+	
+	
+	ST7735_FillScreen(0x0000);
+	Delay100ms(10);
+	
+	
+	while(!gameOverFlag && scoreNum < 10){
+		
+		// draw score to LCD
+		ST7735_DrawString(0, 0, Score_English, 255);
+		scoreNumString = (char) (scoreNum + 48);
+		ST7735_DrawString(7, 0, &scoreNumString, 255 );
+		
 		while(!ADCStatus){}
 		ADCStatus = 0;
 			
@@ -450,47 +572,66 @@ int main(void){
 			ST7735_DrawBitmap( enemySprite.x , enemySprite.y , enemySprite.image, enemySprite.w, enemySprite.h );
 		}
 		
+		if (enemyKilledFlag){
+			
+			scoreNum++;
+			ST7735_DrawBitmap(0, 127, smallSkull, 61, 80);
+			Wave_Killed();
+			Delay100ms(10);
+			enemyKilledFlag = 0;
+			ST7735_FillScreen(0x0000);
+			
+		}
+		
+	
 		ADCStatus = 0;
+	
 	}
 	
+	// player died
+	if (gameOverFlag){
+		Wave_Killed();
+		ST7735_FillScreen(0x0000);
+		Delay100ms(15);
+		ST7735_DrawBitmap(0, 127, smallSkull, 61, 80);
+	}
 	
-	// filler below
-  for(phrase_t myPhrase=HELLO; myPhrase<= GOODBYE; myPhrase++){
-    for(Language_t myL=English; myL<= French; myL++){
-         ST7735_OutString((char *)Phrases[LANGUAGE][myL]);
-      ST7735_OutChar(' ');
-         ST7735_OutString((char *)Phrases[myPhrase][myL]);
-      ST7735_OutChar(13);
-    }
-  }
-  Delay100ms(30);
-  ST7735_FillScreen(0x0000);       // set screen to black
-  l = 128;
-  while(1){
-    Delay100ms(20);
-    for(int j=0; j < 3; j++){
-      for(int i=0;i<16;i++){
-        ST7735_SetCursor(7*j+0,i);
-        ST7735_OutUDec(l);
-        ST7735_OutChar(' ');
-        ST7735_OutChar(' ');
-        ST7735_SetCursor(7*j+4,i);
-        ST7735_OutChar(l);
-        l++;
-      }
-    }
-  }
+	// win
+	if (scoreNum == 10){
+		//Wave_Killed();
+	}
+	
+
+}
 
 
-
-
-
-  
+int Collision(sprite_t sprite1, sprite_t sprite2){
+	
+	// a bit rought calculations since +- pixel won't affect game
+	int sprite1X1 = sprite1.x;
+	int sprite1Y1 = sprite1.y;
+	
+	int sprite1X2 = sprite1.x + sprite1.w;
+	int sprite1Y2 = sprite1.y - sprite1.h;
 	
 	
+	int sprite2X1 = sprite2.x;
+	int sprite2Y1 = sprite2.y;
 	
+	int sprite2X2 = sprite2.x + sprite2.w;
+	int sprite2Y2 = sprite2.y - sprite2.h;
 	
-	
+	return !(sprite1X1 > sprite2X2 || sprite1X2 < sprite2X1 || sprite1Y1 < sprite2Y2 || sprite1Y2 > sprite2Y1);
 	
 }
+
+
+
+
+
+
+
+
+
+
 
